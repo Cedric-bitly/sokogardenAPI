b745e5a59e2961db48c83b4bfc1d5ab67ac9e5f9
Cedric <stormyz22a@gmail.com>
2026-02-19 23:53:55 +0300

    updated codes

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask import *
 # import the pymysql module - it helps us to create a connection between python flask and mysql database
 import pymysql
 
+# import bcrypt
+import bcrypt
+
 # create a flask application and give it a name
 app = Flask(__name__)
 
@@ -21,6 +24,9 @@ def signup():
   #by use of the print function lets print all those details sent with the upcoming request
   #print(username, email, password, phone)
 
+ # hash the password using bcrypt
+  hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
   # establish a connection between flask/python and mysql
   connection = pymysql.connect(host="localhost", user="root", password="",database="sokogardenonline")
 
@@ -33,7 +39,7 @@ def signup():
   sql = "INSERT INTO users(username,email,phone,password) VALUES(%s, %s, %s, %s)"
 
  # create a tuple that will hold all the data gotten from the form
-  data = (username, email, phone, password)
+  data = (username, email, phone, hashed_password)
 
  # by use of the cursor, execute the sql as you replace with the actual values
   cursor.execute(sql, data)
@@ -62,11 +68,11 @@ def signin():
       cursor = connection.cursor(pymysql.cursors.DictCursor)
 
       #structure the sql query that will check whether the email and pasword entered are correct
-      sql = "SELECT * FROM users WHERE email = %s AND password = %s"
+      sql = "SELECT * FROM users WHERE email = %s"
 
 
       #  put the data received from the form into a tuple
-      data = (email, password)
+      data = (email,)
 
       # by use of the cursor execute the sql
       cursor.execute(sql, data)
@@ -76,13 +82,17 @@ def signin():
       #print(count)
 
       #if there are records return it means th
-      if count == 0:
+    if count == 0:
         return jsonify({"message" : "login failed"})
-      else:
-        #there must be a user so we create a variable that will hold the details of the user fetched from the dtabase
+    else:
         user = cursor.fetchone()
-        #return the details to the frontend as well as a message
-        return jsonify({"mesaage" : "User logged in successfully", "user":user})
+    
+    # verify the password against the hash in the database
+    if bcrypt.checkpw(password.encode("utf-8"), user["password"].encode("utf-8")):
+        user.pop("password")  # hide password from response
+        return jsonify({"mesaage" : "User logged in successfully", "user": user})
+    else:
+        return jsonify({"message" : "login failed - wrong password"})
 
 
       # if there are records returned it means the email and the password are correct otherwise it means they are wrong
@@ -92,7 +102,7 @@ def signin():
 
 
 
-    
+  
 
 
 
